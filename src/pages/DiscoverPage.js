3607f8980608dae981912380f4d8e143f514e8e0
karlaevelize <k.evelize@gmail.com>
2021-10-21 16:56:08 +0200

    Dynamic search

--- src/pages/DiscoverPage.js
+++ src/pages/DiscoverPage.js
@@ -6,32 +6,24 @@ const API_KEY = "31ea6e90"
 
 export default function DiscoverPage(){
 
-  const [ movies, setMovies ] = useState([])
+  const [ movies, setMovies ] = useState()
 
-// hardcoded the search term
-// in the Reader, you will use an input field to get it
-  const searchText = "life"
+  //holds what the user types
+  const [ searchText, setSearchText ] = useState("")
 
-  // 1. Write an async function
-  // 2. Make a request with axios
-  // 3. Console.log the response
-  // 4. Call the function inside useEffect
-  // 5. Check the console.log to find the data
-  // 6. Put the data in the state (setState(response.blabla))
-
-  const fetchData = async () => {
+  const getMovies = async () => {
     const response = await axios.get(`http://www.omdbapi.com/?s=${searchText}&apikey=${API_KEY}`)
     console.log("response", response.data.Search)
     setMovies(response.data.Search)
   }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
-
   return(
     <div>
       <h1>Discover Movies</h1>
+      {/* we save what the user types in the the state */}
+      <input value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+      {/* we call the function in the button, instead of useEffect */}
+      <button onClick={getMovies}>Search</button>
       <p>Searchin movies with the word <b>{searchText}</b></p>
       {!movies 
         ? "Loading" 
